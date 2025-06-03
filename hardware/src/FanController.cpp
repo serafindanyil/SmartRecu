85d589395cc7444f58714db3c22c91cec8001519
serafindanyil <danyil.serafin.ir.2023@lpnu.ua>
2025-06-03 03:47:54 +0300

    feat:tahometr to fan

--- hardware/src/FanController.cpp
+++ hardware/src/FanController.cpp
@@ -1,16 +1,62 @@
 #include "FanController.h"
 
-FanController::FanController(uint8_t pwmPin, uint8_t pwmChannel)
-    : _pwmPin(pwmPin), _pwmChannel(pwmChannel) {}
+FanController* FanController::instance = nullptr;
+
+FanController::FanController(uint8_t pwmPin, uint8_t pwmChannel, uint8_t tachPin)
+    : _pwmPin(pwmPin), _pwmChannel(pwmChannel), _tachPin(tachPin) {
+    instance = this;
+}
+
+void IRAM_ATTR FanController::pulseISRStatic() {
+    if (instance) instance->pulseISR();
+}
+
+void IRAM_ATTR FanController::pulseISR() {
+    unsigned long now = micros();
+    // Дебаунс: ігноруємо імпульси, які йдуть частіше ніж 2000 мкс (2 мс)
+    if (now - _lastPulseTime > 2000) {
+        portENTER_CRITICAL_ISR(& _mux);
+        _pulseCount++;
+        portEXIT_CRITICAL_ISR(& _mux);
+        _lastPulseTime = now;
+    }
+}
 
 void FanController::begin() {
-    ledcSetup(_pwmChannel, 25000, 8); // 25kHz, 8-bit
+    ledcSetup(_pwmChannel, 25000, 8);
     ledcAttachPin(_pwmPin, _pwmChannel);
+
+    pinMode(_tachPin, INPUT_PULLUP); // рекомендую зовнішній pull-up 10кОм
+
+    attachInterrupt(digitalPinToInterrupt(_tachPin), pulseISRStatic, FALLING);
+
     setSpeedPercent(0);
+    _lastRPMCalcTime = millis();
 }
 
 void FanController::setSpeedPercent(uint8_t percent) {
     if (percent > 100) percent = 100;
     uint8_t duty = (percent * 255) / 100;
     ledcWrite(_pwmChannel, duty);
+}
+
+void FanController::update() {
+    unsigned long now = millis();
+    if (now - _lastRPMCalcTime >= 1000) {
+        uint16_t pulses;
+        portENTER_CRITICAL(&_mux);
+        pulses = _pulseCount;
+        _pulseCount = 0;
+        portEXIT_CRITICAL(&_mux);
+
+        Serial.print("Pulses counted in 1 sec: ");
+        Serial.println(pulses);
+
+        _rpm = (pulses * 60) / PULSES_PER_REV;
+        _lastRPMCalcTime = now;
+    }
+}
+
+uint16_t FanController::getRPM() {
+    return _rpm;
 }
--- hardware/src/FanController.h
+++ hardware/src/FanController.h
@@ -5,13 +5,30 @@
 
 class FanController {
 public:
-    FanController(uint8_t pwmPin, uint8_t pwmChannel);
+    FanController(uint8_t pwmPin, uint8_t pwmChannel, uint8_t tachPin);
     void begin();
     void setSpeedPercent(uint8_t percent);
+    void update(); // викликати у loop() для обчислення RPM і виводу імпульсів
+    uint16_t getRPM();
 
 private:
     uint8_t _pwmPin;
     uint8_t _pwmChannel;
+    uint8_t _tachPin;
+
+    volatile uint16_t _pulseCount = 0;
+    volatile unsigned long _lastPulseTime = 0; // для debounce у ISR
+
+    unsigned long _lastRPMCalcTime = 0;
+    uint16_t _rpm = 0;
+
+    static constexpr uint8_t PULSES_PER_REV = 2; // 2 імпульси на оберт (для Arctic P12 MAX)
+
+    static FanController* instance; // для статичного ISR
+    static void IRAM_ATTR pulseISRStatic();
+    void IRAM_ATTR pulseISR();
+
+    portMUX_TYPE _mux = portMUX_INITIALIZER_UNLOCKED; // для критичних секцій
 };
 
 #endif
